c7cd2948aa2c0bbef090db22d056d999f2ff5080
ItzelCNE <itzel.cne@gmail.com>
2023-09-30 20:41:56 -0300

    lazyday uuu

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
+from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,relationship
 from sqlalchemy import ForeignKey,text
+from typing import List
 
 
 
@@ -12,11 +13,14 @@ class User(Base):
     Username:Mapped[str] = mapped_column(nullable=False)
     Password:Mapped[int] = mapped_column(nullable=False)
     email:Mapped[str]
+    comment:Mapped[List["Comment"]] = relationship(back_populates='user')
+
 
 class Comment(Base):
     __tablename__ = 'Comments'
     id:Mapped[int] = mapped_column(primary_key=True)
     user_id:Mapped[int] = mapped_column(ForeignKey(User.id))
     text:Mapped[str] = mapped_column(text,nullable=False)
+    user:Mapped["User"] =relationship(back_populates=false)
 
 
